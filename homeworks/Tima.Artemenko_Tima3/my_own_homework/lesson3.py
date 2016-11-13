950a1dc70cf1490fa330f5a4b624e8a1ff076135
Tima3 <artemenkotima9@gmail.com>
2016-11-13 17:47:43 +0200

    added my own homework

---
+++ homeworks/Tima.Artemenko_Tima3/my_own_homework/lesson3.py
@@ -0,0 +1,16 @@
+from datetime import timezone
+
+#Time date were imported by time zone
+
+weather = ("rain")#created variable
+time = ("night")
+
+if weather == "rain" and time == "night":
+    print("the weather is bad today")
+else:
+    print("the weather is awesome")
+if time == "day":
+    print("i can go to play , because i love a dark")
+else:
+    print("we cannot go to play , because time to sleep")
+#the end of the code
